ee745f0d675a26f2bcc8760b352051c374028711
Alex Dameron <AWDameron@gmail.com>
2024-09-27 21:10:31 -0400

    fixed bugs, updated Error handling to work properly

--- FCC Arithmetic Formatter.py
+++ FCC Arithmetic Formatter.py
@@ -2,9 +2,13 @@ def arithmetic_arranger(problems, show_answers=False):
     answers = []
 
     if len(problems) > 5:
-        return "Error: Too many problems."
+        print("Error: Too many problems.")
     
-    top_numbers, operators, bottom_numbers = sort_arithmetic(problems)
+    result = sort_arithmetic(problems)
+    if result is None:
+        return
+    
+    top_numbers, operators, bottom_numbers = sort_arithmetic(problems)    
     answers = solve_arithmetic(top_numbers,operators,bottom_numbers)
     answer_strings = print_arithmetic(top_numbers,operators,bottom_numbers,answers)
     
@@ -17,6 +21,7 @@ def arithmetic_arranger(problems, show_answers=False):
 # This function is designed to take the initial list [problems] and break it into 3 seperate lists.
 # It also doubles as my validation method to make sure the data that is input meets FCC's projects requirements.
 def sort_arithmetic(problems):
+    
     top_numbers = []
     operators = []
     bottom_numbers = []
@@ -25,13 +30,16 @@ def sort_arithmetic(problems):
         top,op,bot = problem.split()
 
         if not top.isdigit() or not bot.isdigit():
-            return "Error: Numbers not only contain digits."
+            print("Error: Numbers not only contain digits.")
+            return 
 
         if op not in ["+","-"]:
-            return "Error: Operator must be '+' or '-'."
+            print("Error: Operator must be '+' or '-'.")
+            return 
         
         if len(top) > 4 or len(bot) > 4:
-            return "Error: Numbers cannot be more than four digits."
+            print("Error: Numbers cannot be more than four digits.")
+            return 
         
         top_numbers.append(top)
         operators.append(op)
@@ -80,5 +88,18 @@ def print_arithmetic(top_numbers,operators,bottom_numbers,answers):
     print('    '.join(dashes))
  
     return answer_strings
-        
-arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"],True)
+
+# class for throwing errors.
+class ArithmeticError(Exception):
+    pass
+
+arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]) 
+arithmetic_arranger(["3801 - 2", "123 + 49"])
+arithmetic_arranger(["1 + 2", "1 - 9380"])
+arithmetic_arranger(["11 + 4", "3801 - 2999", "1 + 2", "123 + 49", "1 - 9380"])
+arithmetic_arranger(["44 + 815", "909 - 2", "45 + 43", "123 + 49", "888 + 40", "653 + 87"])
+arithmetic_arranger(["3 / 855", "3801 - 2", "45 + 43", "123 + 49"]) 
+arithmetic_arranger(["24 + 85215", "3801 - 2", "45 + 43", "123 + 49"])
+arithmetic_arranger(["98 + 3g5", "3801 - 2", "45 + 43", "123 + 49"])
+arithmetic_arranger(["3 + 855", "988 + 40"], True)
+arithmetic_arranger(["32 - 698", "1 - 3801", "45 + 43", "123 + 49", "988 + 40"], True)
